fce9f3855613145c0e968bf1ec1b9272373b3307
Candido Souza <candido_souzza@hotmail.com>
2014-07-10 12:19:00 -0300

    Iniciando o 3º projeto Code Education

---
+++ 02-PHP-FOUNDATION/03-projeto/index.php
@@ -0,0 +1,9 @@
+<?php require_once('pages/header.php');?>
+
+<?php require_once(routeUrl());?>
+
+<?php require_once('pages/footer.php');?>
+
+
+
+
---
+++ 02-PHP-FOUNDATION/03-projeto/pages/header.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+date_default_timezone_set('America/Sao_Paulo');
+
+require_once('functions/functions.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>2º Projeto php Code Education</title>
+    <!-- style -->
+	<link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+							<span class="sr-only">Alterar a navegação</span>
+					       	<span class="icon-bar"></span>
+					       	<span class="icon-bar"></span>
+					       	<span class="icon-bar"></span>
+							</button>
+					    	<a class="navbar-brand" href="home">LOGO</a>
+						</div>
+						<!-- /navbar-header -->
+						<div class="collapse navbar-collapse" id="menu">
+							<ul class="nav navbar-nav">
+			                   	<li><a href="home">Home</a></li>
+								<li><a href="empresa">Empresa</a></li>
+								<li><a href="produtos">Produtos</a></li>
+								<li><a href="servicos">Serviços</a></li>
+								<li><a href="contato">Contato</a></li>
+							</ul>
+						</div>
+						<!-- /collapse navbar-collapse -->
+					</div>
+					<!-- /container-fluid -->
+				</nav>
+				<!-- /nav -->
+			</div>
+			<!-- /row nav -->
+			<div id="content">	
