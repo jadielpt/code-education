eef297d57a0911d5ad145d46aaf51ad8fe7e1c2f
Candido Souza <candido_souzza@hotmail.com>
2014-08-04 16:27:32 -0300

    Criação de classe de endereço de entregas

--- 03-ORIENTACAO-A-OBJETOS/02-projeto/public/pages/clientes.php
+++ 03-ORIENTACAO-A-OBJETOS/02-projeto/public/pages/clientes.php
@@ -12,34 +12,45 @@
 use \src\app\classes\ClientesPessoasFisicas as ClientesPessoasFisicas;
 
 $clientes[0] = new ClientesPessoasFisicas(1, "Candido Souza", "candido@email.com.br", "Pessoa Física", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 5, "Votuporanga", "SP");
-$clientes[0]->setCelular(17977777777);
+$clientes[0]->setCelular(17977777777)
+            ->setTelContato(null)
+            ->setCobrRua(null)
+            ->setCobrNumero(null)
+            ->setCobrComplemento(null)
+            ->setCobrBairro(null)
+            ->setCobrCep(null)
+            ->setCobrMunicipio(null)
+            ->setCobrUf(null)
+;
+
+
 
 $clientes[1] = new \src\app\classes\ClientesPessoasJuridicas(2, "Estofados ME", "estofados@email.com.br", "Pessoa Júridica", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", "Normal", "Votuporanga", "SP");
-$clientes[1]->setFax(17977777777);
+$clientes[1]->setFax(17777777777);
 
 $clientes[2] = new ClientesPessoasFisicas(3, "Claudia Bertolin", "claudia@email.com.br", "Pessoa Física", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 3, "Votuporanga", "SP");
 $clientes[2]->setCelular(17977777777);
 
 $clientes[3] = new \src\app\classes\ClientesPessoasJuridicas(4, "Livros SA", "livros@email.com.br", "Pessoa Júridica", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 4, "Votuporanga", "SP");
-$clientes[3]->setFax(17977777777);
+$clientes[3]->setFax(17777777777);
 
 $clientes[4] = new ClientesPessoasFisicas(5, "Felipe Bertolin de Souza", "felipe@email.com.br", "Pessoa Física", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", "Normal", "Votuporanga", "SP");
 $clientes[4]->setCelular(17977777777);
 
 $clientes[5] = new \src\app\classes\ClientesPessoasJuridicas(6, "Petrobras do Brasil SA", "petrobras@email.com.br", "Pessoa Júridica", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 5, "Votuporanga", "SP");
-$clientes[5]->setFax(17977777777);
+$clientes[5]->setFax(17777777777);
 
 $clientes[6] = new ClientesPessoasFisicas(7, "Maria Helena", "maria@email.com.br", "Pessoa Física", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", "Normal", "Votuporanga", "SP");
 $clientes[6]->setCelular(17977777777);
 
 $clientes[7] = new \src\app\classes\ClientesPessoasJuridicas(8, "Coca Cola SA", "cocacola@email.com.br", "Pessoa Júridica", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 2, "Votuporanga", "SP");
-$clientes[7]->setFax(17977777777);
+$clientes[7]->setFax(17777777777);
 
 $clientes[8] = new ClientesPessoasFisicas(9, "Vitor Souza", "vitor@email.com.br", "Pessoa Física", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 4, "Votuporanga", "SP");
 $clientes[8]->setCelular(17977777777);
 
 $clientes[9] = new \src\app\classes\ClientesPessoasJuridicas(10, "Microsoft do Brasil SA", "micro@email.com.br", "Pessoa Júridica", 77777777777, 32435549, "Piauí", 777, "Centro", 15500000, "casa", 5, "Votuporanga", "SP");
-$clientes[9]->setFax(17977777777);
+$clientes[9]->setFax(17777777777);
 
 //echo '<pre>';
 //print_r($clientes);
--- 03-ORIENTACAO-A-OBJETOS/02-projeto/public/pages/visualizarCliente.php
+++ 03-ORIENTACAO-A-OBJETOS/02-projeto/public/pages/visualizarCliente.php
@@ -10,33 +10,63 @@
 * Linguagem: php
 */
 -->
+<?php
+//echo '<pre>';
+//print_r($clientes[0]);
+//echo '</pre>'
+?>
 <h1>Dados do Cliente</h1>
 <div class="jumbotron">
-<?php
-if(isset($_GET)) {
-    $codigo = array_keys($_GET);
-    
-    foreach ($codigo as $key => $value) {
-        $valor = $value-1;
+    <div class="col-md-12">
 
-        echo "<h4>Código: <strong>".$clientes[$valor]->getId()."</strong></h4>";
-        echo "<h4>Nome: <strong>".$clientes[$valor]->getnomeRS()."</strong></h4>";
-        echo "<h4>E-mail: <strong>".$clientes[$valor]->getEmail()."</strong></h4>";
-        echo "<h4>Cpf | CNPJ: <strong>".$clientes[$valor]->getCnpjCpf()."</strong></h4>";
-        echo "<h4>Tipo: <strong>".$clientes[$valor]->getTipo()."</strong></h4>";
-        echo "<h4>Cliente: <strong>".$clientes[$valor]->getGrauImportance()." Estrelas</strong></h4>";
-        echo "<h4>Telefone: <strong>".$clientes[$valor]->getTelefone()."</strong></h4>";
-        echo "<h4>Rua: <strong>".$clientes[$valor]->getRua()."</strong></h4>";
-        echo "<h4>Nº: <strong>".$clientes[$valor]->getNumero()."</strong></h4>";
-        echo "<h4>Complemento: <strong>".$clientes[$valor]->getComplemento()."</strong></h4>";
-        echo "<h4>Bairro: <strong>".$clientes[$valor]->getBairro()."</strong></h4>";
-        echo "<h4>Cep: <strong>".$clientes[$valor]->getCep()."</strong></h4>";
-        echo "<h4>Municipio: <strong>".$clientes[$valor]->getMunicipio()."</strong></h4>";
-        echo "<h4>UF: <strong>".$clientes[$valor]->getUf()."</strong></h4>";
-    }
-}
-?>
-<a href="index.php"><button class="btn btn-info " >Voltar</button></a>
+        <?php
+        if(isset($_GET)) {
+        $codigo = array_keys($_GET);
+
+            foreach ($codigo as $key => $value) {
+                $valor = $value-1;
+
+                echo "<div class=\"col-md-6\"><h2>Dados do Cliente</h2><br>";
+                echo "<h4>Código: <strong>".$clientes[$valor]->getId()."</strong></h4>";
+                echo "<h4>Nome: <strong>".$clientes[$valor]->getnomeRS()."</strong></h4>";
+                echo "<h4>E-mail: <strong>".$clientes[$valor]->getEmail()."</strong></h4>";
+                echo "<h4>Cpf | CNPJ: <strong>".$clientes[$valor]->getCnpjCpf()."</strong></h4>";
+                echo "<h4>Tipo: <strong>".$clientes[$valor]->getTipo()."</strong></h4>";
+                echo "<h4>Cliente: <strong>".$clientes[$valor]->getGrauImportance()." Estrelas</strong></h4>";
+                echo "<h4>Telefone: <strong>".$clientes[$valor]->getTelefone()."</strong></h4>";
+
+//                if(!$clientes[$valor]->getCelular()){
+//                    echo "<h4>Telefone: <strong>".$clientes[$valor]->getFax()."</strong></h4>";
+//
+//                }else{
+//                    echo "<h4>Celular: <strong>".$clientes[$valor]->getCelular()."</strong></h4>";
+//
+//                }
+                echo "<h4>Rua: <strong>".$clientes[$valor]->getRua()."</strong></h4>";
+                echo "<h4>Nº: <strong>".$clientes[$valor]->getNumero()."</strong></h4>";
+                echo "<h4>Complemento: <strong>".$clientes[$valor]->getComplemento()."</strong></h4>";
+                echo "<h4>Bairro: <strong>".$clientes[$valor]->getBairro()."</strong></h4>";
+                echo "<h4>CEP: <strong>".$clientes[$valor]->getCep()."</strong></h4>";
+                echo "<h4>Municipio: <strong>".$clientes[$valor]->getMunicipio()."</strong></h4>";
+                echo "<h4>UF: <strong>".$clientes[$valor]->getUf()."</strong></h4>";
+
+                echo "</div><div class=\"col-md-6\"> <h2>Endereço para Cobrança</h2><br>";
+
+                echo "<h4>Telefone Contato: <strong>".$clientes[$valor]->getTelContato()."</strong></h4>";
+                echo "<h4>Rua: <strong>".$clientes[$valor]->getCobrRua()."</strong></h4>";
+                echo "<h4>Nº: <strong>".$clientes[$valor]->getCobrNumero()."</strong></h4>";
+                echo "<h4>Complemento: <strong>".$clientes[$valor]->getCobrComplemento()."</strong></h4>";
+                echo "<h4>Bairro: <strong>".$clientes[$valor]->getCobrBairro()."</strong></h4>";
+                echo "<h4>CEP: <strong>".$clientes[$valor]->getCobrCep()."</strong></h4>";
+                echo "<h4>Municipio: <strong>".$clientes[$valor]->getCobrMunicipio()."</strong></h4>";
+                echo "<h4>UF: <strong>".$clientes[$valor]->getCobrUf()."</strong></h4>";
+
+                echo "</div>";
+            }
+        }
+        ?>
+    </div>
+        <a href="index.php"><button class="btn btn-info " >Voltar</button></a>
 </div><!-- /jumbotron -->
 
 
