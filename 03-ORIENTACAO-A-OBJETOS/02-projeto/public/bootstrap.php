86bffb8ba75008f23c05d08e446df8429094f0d6
Candido Souza <candido_souzza@hotmail.com>
2014-08-05 07:32:12 -0300

    Correção de errors.log

--- 03-ORIENTACAO-A-OBJETOS/02-projeto/public/bootstrap.php
+++ 03-ORIENTACAO-A-OBJETOS/02-projeto/public/bootstrap.php
@@ -11,7 +11,7 @@
  */
 
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set("display_errors", 0);
 ini_set("log_errors", 1);
 ini_set("error_log", "../errors.log");
 date_default_timezone_set('America/Sao_Paulo');
