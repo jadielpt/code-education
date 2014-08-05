7e566129e259e914645a4caad2effb7e3061b1de
Candido Souza <candido_souzza@hotmail.com>
2014-08-05 16:50:21 -0300

    Iniciando 03 projeto Refatoração do curso Trilhando Caminho com Php - Portal Code Education

---
+++ 03-ORIENTACAO-A-OBJETOS/03-projeto/src/CandidoSouza/Clientes/Abstracts/ClienteAbstract.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: candidosouza
+ * Date: 05/08/14
+ * Time: 16:45
+ */
+
+namespace src\CandidoSouza\Abstracts;
+
+
+abstract class ClienteAbstract {
+
+} 
---
+++ 03-ORIENTACAO-A-OBJETOS/03-projeto/src/CandidoSouza/Clientes/Utils/ClienteTrait.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: candidosouza
+ * Date: 05/08/14
+ * Time: 16:42
+ */
+
+namespace src\CandidoSouza\Utils;
+
+
+trait ClienteTrait {
+
+} 
