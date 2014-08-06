d365b61499169d7e764422dbd2c76de000ac901c
Candido Souza <candido_souzza@hotmail.com>
2014-08-06 11:23:17 -0300

    Corrigindo arquitetura de pastas

---
+++ 03-ORIENTACAO-A-OBJETOS/03-projeto/src/CandidoSouza/Applications/pages/404.php
@@ -0,0 +1,18 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: candidosouza
+ * Date: 01/08/14
+ * Time: 16:12
+ * 02 - Projeto Tipos de Clientes | Estudos Potal Code Education - Módulo 03 Php Orientado a Objetos
+ * @author Candido Souza
+ * Arquivo: 404.php
+ * Linguagem: php
+ */
+-->
+<h1>ERRO: A página não existe</h1>
+<div class="jumbotron">
+    <h1>Ixi, Maria! ERROR: 404</h1>
+    <h2>ERRO: A página não existe</h2>
+    <h2>Volte para a <a href="index.php">Home</a></h2>
+</div>
