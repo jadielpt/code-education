4f6a73e51edc83fc54bb64cb0ff2512c448404e8
Candido Souza <candido_souzza@hotmail.com>
2014-08-06 08:57:32 -0300

    Corrigindo estruturas de pasta

---
+++ 03-ORIENTACAO-A-OBJETOS/03-projeto/public/pages/footer.php
@@ -0,0 +1,20 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: candidosouza
+ * Date: 01/08/14
+ * Time: 16:12
+ * 02 - Projeto Tipos de Clientes | Estudos Potal Code Education - Módulo 03 Php Orientado a Objetos
+ * @author Candido Souza
+ * Arquivo: footer.php
+ * Linguagem: php
+ */
+-->
+                    <footer>
+                        <p>04 Projeto - Code Education | &copy Todos os direitos reservados 2014 - <?php echo date('Y');?></p>
+                    </footer>
+                </div><!-- /col-md-12 -->
+            </div><!-- /row -->
+        </div><!-- /container-fluid -->
+    </body>
+</html>
