998dc3e7661618d089365f7ded41c2779a6cb1ae
Candido Souza <candido_souzza@hotmail.com>
2014-11-04 16:17:37 -0200

    Corrigindo a classe products entity

--- 06-APIS-E-SILEX/02-projeto/src/APIsSilex/Products/Entity/Products.php
+++ 06-APIS-E-SILEX/02-projeto/src/APIsSilex/Products/Entity/Products.php
@@ -4,34 +4,12 @@ namespace APIsSilex\Products\Entity;
 
 use APIsSilex\Products\Interfaces\ProductsInterface;
 
-use Doctrine\ORM\Mapping  as ORM;
-
-/**
- * @ORM\Entity
- * @ORM\Table(name="products")
- */
 class Products implements ProductsInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue
-     */
+
     private $id;
-    
-    /**
-     * @ORM\Column(type="text")
-     */
     private $name;
-    
-    /**
-     * @ORM\Column(type="text")
-     */
     private $description;
-    
-    /**
-     * @ORM\Column(type="text")
-     */
     private $value;
     
     function getId() {
