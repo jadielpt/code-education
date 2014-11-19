dd83b1ed50fbc03502a4357cc29d050a7faa6b17
Candido Souza <candido_souzza@hotmail.com>
2014-11-19 21:14:34 -0200

    Finalizando a listagem, listagem por id, e insert de produtos com doctrine

--- 07-DOCTRINE/01-projeto/src/Products/Mapper/ProductsMapperApi.php
+++ 07-DOCTRINE/01-projeto/src/Products/Mapper/ProductsMapperApi.php
@@ -21,43 +21,44 @@ class ProductsMapperApi implements ProductsMapperApiInterface
 
     public function fetchAll()
     {
-        return $this->em->getRepository('Products\Entity\ProductsApi')->findAll();
-        // Documentação = http://doctrine-orm.readthedocs.org/en/latest/reference/working-with-objects.html
+        try{
+            $result = $this->em->getRepository('Products\Entity\ProductsApi')->findAll();
+        } catch (PDOException $e) {
+            echo "ERROR: Unable to list the data in the database!";
+            die("Code: {$e->getCode()} <br> Message: {$e->getMessage()} <br>  File: {$e->getFile()} <br> Line: {$e->getLine()}");
+        }
+        return $result;
+        // Documentação = http://doctrine-orm.readthedocs.org/en/latest/reference/working-with-objects.html 8.8. Querying
     }
     
     public function findOneById($id)
     {
-        try {
-            $conn = Connect::getDb();
-            $product = $conn->prepare("SELECT * FROM products WHERE id = :id");
-            $product->bindValue("id", $id);
-            $product->execute();
-            $result = $product->fetch(\PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            echo "ERROR: Unable to perform query!";
-            die("Code: {$e->getCode()} <br> Message: {$e->getMessage()} <br>  File: {$e->getFile()} <br> Line: {$e->getLine()}");
-        }
-        
-        return $result;
-    }
-
-    public function insert(ProductsApiInterface $products)
-    {
         try{
-            Registry::set('connections', Connect::getDb());
-            $conn = Registry::get('connections');
-            $list = $conn->prepare("INSERT INTO products (name, description , value) VALUES (:name, :description , :value)");
-            $data = $list->execute(array(
-                ':name'        => $products->getName(),
-                ':description' => $products->getDescription(),
-                ':value'       => $products->getValue()
-            ));
+            $result =  $this->em->find('Products\Entity\ProductsApi', $id);
         } catch (PDOException $e) {
             echo "ERROR: Unable to list the data in the database!";
             die("Code: {$e->getCode()} <br> Message: {$e->getMessage()} <br>  File: {$e->getFile()} <br> Line: {$e->getLine()}");
         }
 
-        return $data;
+        return $result;
+        // Documentação = http://doctrine-orm.readthedocs.org/en/latest/reference/working-with-objects.html 8.8. Querying
+    }
+
+    public function insert(ProductsApiInterface $products)
+    {
+        try{
+            $this->em->persist($products);
+            $this->em->flush();
+        } catch (PDOException $e) {
+            echo "ERROR: Unable to list the data in the database!";
+            die("Code: {$e->getCode()} <br> Message: {$e->getMessage()} <br>  File: {$e->getFile()} <br> Line: {$e->getLine()}");
+        }
+
+        $this->em->clear();
+
+        return true;
+        // documentação = http://doctrine-orm.readthedocs.org/en/latest/tutorials/getting-started.html
+        // documentação = http://doctrine-orm.readthedocs.org/en/latest/reference/batch-processing.html
     }
 
     public function update(ProductsApiInterface $products)
